385ab12cbb25d7038cd4b8d3bff92f1fe95154bf
Kayla <119716033+kaylakintchen@users.noreply.github.com>
2023-06-17 23:59:47 -0400

    Add files via upload

---
+++ SslServerApplication.java
@@ -0,0 +1,41 @@
+package com.snhu.sslserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+
+@SpringBootApplication
+public class SslServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SslServerApplication.class, args);
+    }
+}
+
+@RestController
+class ServerController {
+    public static String calculateHash(String name) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        byte[] hash = md.digest(name.getBytes(StandardCharsets.UTF_8));
+        BigInteger number = new BigInteger(1, hash);
+        StringBuilder hexString = new StringBuilder(number.toString(16));
+        while (hexString.length() < 32) {
+            hexString.insert(0, '0');
+        }
+        return hexString.toString();
+    }
+
+    @RequestMapping("/hash")
+    public String myHash() throws NoSuchAlgorithmException {
+        String data = "Kayla Kintchen";
+        String hash = calculateHash(data);
+        return "<p>data:" + data + " <p>Name of Cipher Used: SHA-256 " + " <p>Checksum Value: " + hash;
+    }
+}
+
+ 
